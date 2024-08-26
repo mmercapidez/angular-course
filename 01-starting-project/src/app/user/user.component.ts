9b7b7a87984d48b35ba341b0effc1d92312e665f
Martín Mercapidez <mmercapidez@mtss.gub.uy>
2024-08-25 21:07:08 -0300

    28.Introducing Signals

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dumm-data/dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -12,15 +12,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  //computed properties with signals (this is becouse angular recompute the property)
+  imagePath = computed(() => '../../assets/users/' + this.selectedUser().avatar);
 
-  public get imagePath() {
-    return '../../assets/users/' + this.selectedUser.avatar;
-  }
+  // public get imagePath() {
+  //   return '../../assets/users/' + this.selectedUser.avatar;
+  // }
 
   onSelectUser() {
     console.log('clicked!');
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    //this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
