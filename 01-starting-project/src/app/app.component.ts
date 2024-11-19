76f936b6a6a2472cc5329a254146ddefd40754c4
Martín Mercapidez <mmercapidez@mtss.gub.uy>
2024-11-19 16:41:29 -0300

    43.Legacy Angular: Using ngFor & ngIf

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dumm-data/dummy-users';
@@ -8,7 +10,7 @@ import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
